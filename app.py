94856219f85a38ef91beeab0111620382bf4bc9a
Marek Ventur <venturmv@amazon.com>
2021-05-14 15:04:22 +0100

    webhook challenge response

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 import os
-from bottle import route, run
+from bottle import route, run, request
+import json
 
-@route('/hello/:name')
+@route('/slack-webhook', method='POST')
 def index(name='World'):
-    return '<b>Hello %s!</b>' % name
+  data = request.json
+  if "challenge" in data:
+    return data['challenge']
+  print("Unsure what to do with", data)
+  return "?"
 
 
 if __name__ == '__main__':
-    # Get required port, default to 5000.
-    port = os.environ.get('PORT', 5000)
+  # Get required port, default to 5000.
+  port = os.environ.get('PORT', 5000)
 
-    # Run the app.
-    run(host='0.0.0.0', port=port)
+  # Run the app.
+  run(host='0.0.0.0', port=port)
